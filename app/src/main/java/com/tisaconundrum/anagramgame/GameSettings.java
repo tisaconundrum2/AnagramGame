e5394ebd9760d194057215c38e7747474e0ea8e5
Nicholas Finch <ngf4@nau.edu>
2016-10-17 08:35:05 -0700

    new file:   app/src/main/java/com/tisaconundrum/anagramgame/GameSettings.java 	Adding all the necessary variable settings modified:   app/src/main/java/com/tisaconundrum/anagramgame/MainMenu.java 	Adding initMenu() code, mostly animations new file:   app/src/main/res/anim/back_anim_for_title_left.xml new file:   app/src/main/res/anim/back_anim_for_title_middle.xml new file:   app/src/main/res/anim/back_anim_for_title_right.xml 	Not really necessary Animations that make title look fancy...

---
+++ app/src/main/java/com/tisaconundrum/anagramgame/GameSettings.java
@@ -0,0 +1,17 @@
+package com.tisaconundrum.anagramgame;
+
+/**
+ * Created by tisaconundrum on 10/17/2016.
+ */
+
+public class GameSettings {
+    public static final int ANIMATION_OPEN_BUTTON_DURATION = 400;
+    public static final int ANIMATION_CLOSE_BUTTON_DURATION = 400;
+    public static final int ANIMATION_SHOW_HOME_BUTTON_DURATION = 500;
+    public static final int ANIMATION_HIDE_HOME_BUTTON_DURATION = 500;
+    public static final int ANIMATION_SHOW_TITLE_DURATION = 400;
+    public static final int ANIMATION_HIDE_TITLE_DURATION = 400;
+    public static final String PLAYER_SCORE = "Score";
+    public static final String HIGH_SCORE = "HighScoreAnagram";
+
+}
--- app/src/main/java/com/tisaconundrum/anagramgame/MainMenu.java
+++ app/src/main/java/com/tisaconundrum/anagramgame/MainMenu.java
@@ -3,10 +3,18 @@ package com.tisaconundrum.anagramgame;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainMenu extends AppCompatActivity {
     private RelativeLayout anagramLayout;
+    private Animation compileAnim;
+    private TextView playBtn;
+    private TextView titleLeft;
+    private TextView titleMiddle;
+    private TextView titleRight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,13 +29,84 @@ public class MainMenu extends AppCompatActivity {
         if (getSupportActionBar() != null){
             getSupportActionBar().hide();
         }
-
+        initMenu();
 
     }
 
     private void initCredits(){}
     private void initGame(){}
-    private void initMenu(){}
+    private void initMenu(){
+        titleLeft = (TextView) findViewById(R.id.anagram_left);
+        titleMiddle = (TextView) findViewById(R.id.anagram_middle);
+        titleRight = (TextView) findViewById(R.id.anagram_right);
+
+        // Set up animation for title left
+        compileAnim = AnimationUtils.loadAnimation(MainMenu.this, R.anim.back_anim_for_title_left);
+        compileAnim.setDuration(GameSettings.ANIMATION_HIDE_TITLE_DURATION);
+        compileAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        titleLeft.startAnimation(compileAnim);
+
+        // Set up animation for title middle
+        compileAnim = AnimationUtils.loadAnimation(MainMenu.this, R.anim.back_anim_for_title_middle);
+        compileAnim.setDuration(GameSettings.ANIMATION_HIDE_TITLE_DURATION);
+        compileAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        titleMiddle.startAnimation(compileAnim);
+
+        // Set up animation for title right
+        compileAnim = AnimationUtils.loadAnimation(MainMenu.this, R.anim.back_anim_for_title_right);
+        compileAnim.setDuration(GameSettings.ANIMATION_HIDE_TITLE_DURATION);
+        compileAnim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        titleRight.startAnimation(compileAnim);
+
+    }
     private void initSettings(){}
     public void onBackPressed(){}
 }
