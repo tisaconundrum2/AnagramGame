9aed4c374a89dd5674c190135442195197a43a64
Nicholas Finch <ngf4@nau.edu>
2016-10-17 17:06:27 -0700

    Added title() 	modified:   app/src/main/java/com/tisaconundrum/anagramgame/Settings.java

--- app/src/main/java/com/tisaconundrum/anagramgame/Settings.java
+++ app/src/main/java/com/tisaconundrum/anagramgame/Settings.java
@@ -3,7 +3,10 @@ package com.tisaconundrum.anagramgame;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 /**
  * Created by tisaconundrum on 10/17/2016.
@@ -11,6 +14,10 @@ import android.widget.RelativeLayout;
 public class Settings extends AppCompatActivity{
 
     private RelativeLayout settingsLayout;
+    private TextView titleLeft;
+    private TextView titleMiddle;
+    private TextView titleRight;
+    private Animation compileAnimation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,5 +30,75 @@ public class Settings extends AppCompatActivity{
             getSupportActionBar().hide();
         }
         settingsLayout = (RelativeLayout) findViewById(R.id.settings_layout);
+        title();
+    }
+
+    private void title() {
+        titleLeft = (TextView) findViewById(R.id.anagram_left);
+        titleMiddle = (TextView) findViewById(R.id.anagram_middle);
+        titleRight = (TextView) findViewById(R.id.anagram_right);
+
+        compileAnimation = AnimationUtils.loadAnimation(Settings.this, R.anim.back_anim_for_title_left);
+        compileAnimation.setDuration(GameSettings.ANIMATION_HIDE_TITLE_DURATION);
+        compileAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        titleLeft.startAnimation(compileAnimation);
+
+        compileAnimation = AnimationUtils.loadAnimation(Settings.this, R.anim.back_anim_for_title_right);
+        compileAnimation.setDuration(GameSettings.ANIMATION_HIDE_TITLE_DURATION);
+        compileAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        titleRight.startAnimation(compileAnimation);
+
+        compileAnimation = AnimationUtils.loadAnimation(Settings.this, R.anim.back_anim_for_title_middle);
+        compileAnimation.setDuration(GameSettings.ANIMATION_HIDE_TITLE_DURATION);
+        compileAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+        titleMiddle.startAnimation(compileAnimation);
     }
 }
