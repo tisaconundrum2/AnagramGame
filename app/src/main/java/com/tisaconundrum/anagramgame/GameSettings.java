b8ccf3328829d78186ca845b5b88aaa0aa5714cd
Nicholas Finch <ngf4@nau.edu>
2016-10-18 10:05:34 -0700

    modified:   app/src/main/java/com/tisaconundrum/anagramgame/GameSettings.java 	Added some more settings to reduce the amount of errors I have to see modified:   app/src/main/java/com/tisaconundrum/anagramgame/Settings.java 	Added functionality to musicOnOff() new file:   app/src/main/res/anim/anim_for_no_button.xml 	animation for button modified:   app/src/main/res/layout/settings.xml 	fixed naming convetion

--- app/src/main/java/com/tisaconundrum/anagramgame/GameSettings.java
+++ app/src/main/java/com/tisaconundrum/anagramgame/GameSettings.java
@@ -5,13 +5,27 @@ package com.tisaconundrum.anagramgame;
  */
 
 public class GameSettings {
+    // flash animation for the app
     public static final int ANIMATION_OPEN_BUTTON_DURATION = 400;
     public static final int ANIMATION_CLOSE_BUTTON_DURATION = 400;
     public static final int ANIMATION_SHOW_HOME_BUTTON_DURATION = 500;
     public static final int ANIMATION_HIDE_HOME_BUTTON_DURATION = 500;
     public static final int ANIMATION_SHOW_TITLE_DURATION = 400;
     public static final int ANIMATION_HIDE_TITLE_DURATION = 400;
-    public static final String PLAYER_SCORE = "Score";
-    public static final String HIGH_SCORE = "HighScoreAnagram";
+
+    public static final String PREFS_NAME = "AnagPreferences";
+
+    public static final int GAME_THREAD = 80;
+
+    public static final int SWIPE_THRESH_HOLD = 100;
+    public static final int SWIPE_VELOCITY_THRESH_HOLD = 100;
+    public static final int LAYOUT_PADDING = 0;
+    public static final int LAYOUT_MARGIN = 0;
+
     public static final int START_NEW_ACTIVITY_DURATION = 200;
+
+    public static final String PLAYER_SCORE = "Score";
+    public static final String HIGH_SCORE   = "HighScore";
+    public static final String PLAY_MUSIC = "PlayMusic";
+
 }
--- app/src/main/java/com/tisaconundrum/anagramgame/Settings.java
+++ app/src/main/java/com/tisaconundrum/anagramgame/Settings.java
@@ -1,10 +1,13 @@
 package com.tisaconundrum.anagramgame;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -19,6 +22,8 @@ public class Settings extends AppCompatActivity {
     private TextView titleMiddle;
     private TextView titleRight;
     private Animation compileAnimation;
+    private ImageView musicButton;
+    private boolean isMusicOn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,7 +44,51 @@ public class Settings extends AppCompatActivity {
     }
 
     private void musicOnOff() {
+        musicButton = (ImageView) findViewById(R.id.music_icon);
+        compileAnimation = AnimationUtils.loadAnimation(Settings.this, R.anim.anim_for_no_button);
+        compileAnimation.setDuration(GameSettings.ANIMATION_OPEN_BUTTON_DURATION);
+        compileAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
 
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                SharedPreferences preferences = getApplicationContext()
+                        .getSharedPreferences(GameSettings.PREFS_NAME, Context.MODE_PRIVATE);
+                isMusicOn = preferences.getBoolean(GameSettings.PLAY_MUSIC, true);
+                if (isMusicOn) {
+                    musicButton.setImageResource(R.mipmap.sound_on);
+                } else {
+                    musicButton.setImageResource(R.mipmap.sound_off);
+                }
+                musicButton.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        musicButton.setImageDrawable(null);
+                        if (isMusicOn){
+                            isMusicOn = false;
+                            musicButton.setImageResource(R.mipmap.sound_off);
+                        }else{
+                            isMusicOn = true;
+                            musicButton.setImageResource(R.mipmap.sound_on);
+                        }
+                        SharedPreferences preferences = getApplicationContext()
+                                .getSharedPreferences(GameSettings.PREFS_NAME, Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = preferences.edit();
+                        editor.putBoolean(GameSettings.PLAY_MUSIC, isMusicOn);
+                        editor.commit();
+                    }
+                });
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
     }
 
     private void titleAnimate() {
