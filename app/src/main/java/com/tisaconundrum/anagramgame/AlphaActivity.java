303b7a222e707163ce3a0cbd5ee4ed49069c645b
Nicholas Finch <ngf4@nau.edu>
2016-10-20 01:07:55 -0700

    new file:   app/src/main/java/com/tisaconundrum/anagramgame/AlphaActivity.java 	Adding all letters modified:   app/src/main/res/layout/game_play.xml 	Adding arrow to game_play new file:   app/src/main/res/mipmap-hdpi/left_arrow.png new file:   app/src/main/res/mipmap-hdpi/right_arrow.png 	adding new images

---
+++ app/src/main/java/com/tisaconundrum/anagramgame/AlphaActivity.java
@@ -0,0 +1,43 @@
+package com.tisaconundrum.anagramgame;
+
+import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
+
+/**
+ * Created by tisaconundrum on 10/20/2016.
+ */
+
+public class AlphaActivity extends Activity implements View.OnClickListener{
+    Button alpha_a, alpha_b, alpha_c, alpha_d, alpha_e, alpha_f, alpha_g, alpha_h, alpha_i, alpha_j, alpha_k, alpha_l, alpha_m, alpha_n, alpha_o, alpha_p, alpha_q, alpha_r, alpha_s, alpha_t, alpha_u, alpha_v, alpha_w, alpha_x, alpha_y, alpha_z;
+    @Override
+    alpha_a = (Button) findViewById(R.id.alpha_a);
+    alpha_b = (Button) findViewById(R.id.alpha_b);
+    alpha_c = (Button) findViewById(R.id.alpha_c);
+    alpha_d = (Button) findViewById(R.id.alpha_d);
+    alpha_e = (Button) findViewById(R.id.alpha_e);
+    alpha_f = (Button) findViewById(R.id.alpha_f);
+    alpha_g = (Button) findViewById(R.id.alpha_g);
+    alpha_h = (Button) findViewById(R.id.alpha_h);
+    alpha_i = (Button) findViewById(R.id.alpha_i);
+    alpha_j = (Button) findViewById(R.id.alpha_j);
+    alpha_k = (Button) findViewById(R.id.alpha_k);
+    alpha_l = (Button) findViewById(R.id.alpha_l);
+    alpha_m = (Button) findViewById(R.id.alpha_m);
+    alpha_n = (Button) findViewById(R.id.alpha_n);
+    alpha_o = (Button) findViewById(R.id.alpha_o);
+    alpha_p = (Button) findViewById(R.id.alpha_p);
+    alpha_q = (Button) findViewById(R.id.alpha_q);
+    alpha_r = (Button) findViewById(R.id.alpha_r);
+    alpha_s = (Button) findViewById(R.id.alpha_s);
+    alpha_t = (Button) findViewById(R.id.alpha_t);
+    alpha_u = (Button) findViewById(R.id.alpha_u);
+    alpha_v = (Button) findViewById(R.id.alpha_v);
+    alpha_w = (Button) findViewById(R.id.alpha_w);
+    alpha_x = (Button) findViewById(R.id.alpha_x);
+    alpha_y = (Button) findViewById(R.id.alpha_y);
+    alpha_z = (Button) findViewById(R.id.alpha_z);
+    public void onClick(View v) {
+
+    }
+}
