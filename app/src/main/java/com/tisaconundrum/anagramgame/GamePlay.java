ec9a270bc3f2050383bce9341412f2cf8cda29d9
Nicholas Finch <ngf4@nau.edu>
2016-10-21 02:05:34 -0700

    If this all goes off without a hitch, this program should populate the empty tiles with letters

--- app/src/main/java/com/tisaconundrum/anagramgame/GamePlay.java
+++ app/src/main/java/com/tisaconundrum/anagramgame/GamePlay.java
@@ -72,7 +72,8 @@ public class GamePlay extends AppCompatActivity {
             InputStreamReader inputs = new InputStreamReader(getAssets().open(file));                   // input the anagrams.txt file that will be in the src/main/assets directory
             BufferedReader br = new BufferedReader(inputs);                                             // Read the inputs in
             words = br.readLine().split(" ");                                                           // start counting how many words there. We can tell this by the spaces between each word
-            textViewAnagramWord.setText(words[0]);                                                      // TODO, this sets the Anagram text, what we need to do is call a function to populate the tiles when this ran (Line 84)
+            //textViewAnagramWord.setText(words[0]);                                                    // TODO, this sets the Anagram text, what we need to do is call a function to populate the tiles when this ran (Line 84)
+            getTilePlacement(words[0]);                                                                 // If this all goes off without a hitch, this program should populate the empty tiles with letters
             words[0] = null;                                                                            // delete the existence of the word, because we don't want to be reusing it
             totalwords = words.length - 1;                                                              // get our total word count by using the length function
             textViewNumTokens.setText(wordsfound + "/" + totalwords);                                   // in the xml file textViewNumTokens will now say something like 0/152
@@ -86,8 +87,113 @@ public class GamePlay extends AppCompatActivity {
         ImageView[] tiles = {tile1, tile2, tile3, tile4, tile5, tile6, tile7};                          // put all these variables in a list. XD I can't believe this logic works TBH >_<
         int i = 1;
         for (char ch: string.toCharArray()){                                                            // Parse the string, get each individual letter
-            String tileSetup = "alpha_" + ch;                                                           // setup the string
-            tiles[i].setImageResource(tile[]);
+            switch (ch){                                                                                // You have no idea how mad I am that I have to do it this way...
+                case 'a':                                                                               // Anyway, each number corresponds to a specific letter in R.mipmap
+                    tiles[i].setImageResource(tile[0]);                                                 // Naming convention is the worse... This really should be remedied...
+                    i++;                                                                                // and i'm probably honestly not going to fix this...¯\_(ツ)_/¯
+                    break;
+                case 'b':
+                    tiles[i].setImageResource(tile[2-1]);
+                    i++;
+                    break;
+                case 'c':
+                    tiles[i].setImageResource(tile[3-1]);
+                    i++;
+                    break;
+                case 'd':
+                    tiles[i].setImageResource(tile[4-1]);
+                    i++;
+                    break;
+                case 'e':
+                    tiles[i].setImageResource(tile[5-1]);
+                    i++;
+                    break;
+                case 'f':
+                    tiles[i].setImageResource(tile[6-1]);
+                    i++;
+                    break;
+                case 'g':
+                    tiles[i].setImageResource(tile[7-1]);
+                    i++;
+                    break;
+                case 'h':
+                    tiles[i].setImageResource(tile[8-1]);
+                    i++;
+                    break;
+                case 'i':
+                    tiles[i].setImageResource(tile[9-1]);
+                    i++;
+                    break;
+                case 'j':
+                    tiles[i].setImageResource(tile[10-1]);
+                    i++;
+                    break;
+                case 'k':
+                    tiles[i].setImageResource(tile[11-1]);
+                    i++;
+                    break;
+                case 'l':
+                    tiles[i].setImageResource(tile[12-1]);
+                    i++;
+                    break;
+                case 'm':
+                    tiles[i].setImageResource(tile[13-1]);
+                    i++;
+                    break;
+                case 'n':
+                    tiles[i].setImageResource(tile[14-1]);
+                    i++;
+                    break;
+                case 'o':
+                    tiles[i].setImageResource(tile[15-1]);
+                    i++;
+                    break;
+                case 'p':
+                    tiles[i].setImageResource(tile[16-1]);
+                    i++;
+                    break;
+                case 'q':
+                    tiles[i].setImageResource(tile[17-1]);
+                    i++;
+                    break;
+                case 'r':
+                    tiles[i].setImageResource(tile[18-1]);
+                    i++;
+                    break;
+                case 's':
+                    tiles[i].setImageResource(tile[19-1]);
+                    i++;
+                    break;
+                case 't':
+                    tiles[i].setImageResource(tile[20-1]);
+                    i++;
+                    break;
+                case 'u':
+                    tiles[i].setImageResource(tile[21-1]);
+                    i++;
+                    break;
+                case 'v':
+                    tiles[i].setImageResource(tile[22-1]);
+                    i++;
+                    break;
+                case 'w':
+                    tiles[i].setImageResource(tile[23-1]);
+                    i++;
+                    break;
+                case 'x':
+                    tiles[i].setImageResource(tile[24-1]);
+                    i++;
+                    break;
+                case 'y':
+                    tiles[i].setImageResource(tile[25-1]);
+                    i++;
+                    break;
+                case 'z':
+                    tiles[i].setImageResource(tile[26-1]);
+                    i++;
+                    break;
+            }
+
         }
 
     }
