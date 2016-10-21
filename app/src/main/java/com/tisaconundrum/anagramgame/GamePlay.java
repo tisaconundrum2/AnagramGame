7612877da0403fb7ebfad29cfeb6e169b2572779
Nicholas Finch <ngf4@nau.edu>
2016-10-21 01:46:31 -0700

    modified:   app/src/main/java/com/tisaconundrum/anagramgame/AlphaActivity.java 	This is useless, as it's being done inside GamePlay anyways modified:   app/src/main/java/com/tisaconundrum/anagramgame/GamePlay.java 	Adding a ridiculous amount of code, mostly working out how to get the tiles to be clickable deleted:    app/src/main/res/drawable/alpha_a.xml 	useless file deleted modified:   app/src/main/res/layout/game_play.xml 	Adding Text that says Words matched 0/0

--- app/src/main/java/com/tisaconundrum/anagramgame/GamePlay.java
+++ app/src/main/java/com/tisaconundrum/anagramgame/GamePlay.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -18,43 +17,53 @@ import java.io.InputStreamReader;
  */
 
 public class GamePlay extends AppCompatActivity {
-    private String DATA_FILE = "anagrams.txt"
+    private String DATA_FILE = "anagrams.txt";
     private Integer totalwords = 0;
     private Integer wordsfound = 0;
     private String[] words;
-    private TextView textViewNumTokens;
     private TextView textViewAnagramWord;
     private TextView textViewWordLabel;
     private TextView textViewUsernameTitle;
     private String username;
     private RelativeLayout gamePlayLayout;
     private Button alpha_a, alpha_b, alpha_c, alpha_d, alpha_e, alpha_f, alpha_g, alpha_h, alpha_i, alpha_j, alpha_k, alpha_l, alpha_m, alpha_n, alpha_o, alpha_p, alpha_q, alpha_r, alpha_s, alpha_t, alpha_u, alpha_v, alpha_w, alpha_x, alpha_y, alpha_z;
-    private String[] alphaStrings = new String[]{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
+    private int[] tile = {R.mipmap.alpha_a, R.mipmap.alpha_b, R.mipmap.alpha_c, R.mipmap.alpha_d, R.mipmap.alpha_e, R.mipmap.alpha_f, R.mipmap.alpha_g, R.mipmap.alpha_h, R.mipmap.alpha_i, R.mipmap.alpha_j, R.mipmap.alpha_k, R.mipmap.alpha_l, R.mipmap.alpha_m, R.mipmap.alpha_n, R.mipmap.alpha_o, R.mipmap.alpha_p, R.mipmap.alpha_q, R.mipmap.alpha_r, R.mipmap.alpha_s, R.mipmap.alpha_t, R.mipmap.alpha_u, R.mipmap.alpha_v, R.mipmap.alpha_w, R.mipmap.alpha_x, R.mipmap.alpha_y, R.mipmap.alpha_z};
+    private String[] alphaStrings = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"};
     private Button[] buttons;
-    ImageView buttonSubmit = (ImageView) findViewById(R.id.submit_button);
+    private ImageView buttonSubmit = (ImageView) findViewById(R.id.submit_button);
+    private TextView textViewNumTokens = (TextView)findViewById(R.id.words_matched);
+    private ImageView tile1 = (ImageView) findViewById(R.id.tile1);                                     // Let's
+    private ImageView tile2 = (ImageView) findViewById(R.id.tile2);                                     // be
+    private ImageView tile3 = (ImageView) findViewById(R.id.tile3);                                     // completely
+    private ImageView tile4 = (ImageView) findViewById(R.id.tile4);                                     // honest
+    private ImageView tile5 = (ImageView) findViewById(R.id.tile5);                                     // this
+    private ImageView tile6 = (ImageView) findViewById(R.id.tile6);                                     // isn't
+    private ImageView tile7 = (ImageView) findViewById(R.id.tile7);                                     // optimal
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.game_play);
-        gamePlayLayout = (RelativeLayout) findViewById(R.id.game_play_layout);
-        View decorView = getWindow().getDecorView();
-        int uiOptions = View.SYSTEM_UI_FLAG_FULLSCREEN;
-        decorView.setSystemUiVisibility(uiOptions);
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().hide();
+        super.onCreate(savedInstanceState);                                                             // tell the Dalvik VM to run your code in addition to the existing code in the onCreate() of the parent class.
+        setContentView(R.layout.game_play);                                                             // set xml file of focus
+        gamePlayLayout = (RelativeLayout) findViewById(R.id.game_play_layout);                          // get xml file's android:id. I'll be honest, I'm not sure why this needs to be here
+        View decorView = getWindow().getDecorView();                                                    // Retrieve the current decor view
+        decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN);                                // Make our game full screen, so we don't have to worry about the top bar
+        if (getSupportActionBar() != null) {                                                            // this is redundant, but it's for safety
+            getSupportActionBar().hide();                                                               // simply make sure that the bar is actually hidden, despite being in "Full Screen"
         }
-        getInputFile(DATA_FILE);
+        initTiles();                                                                                    // initialize all our tiles, and get them prepped to be worked with
+        getInputFile(DATA_FILE);                                                                        // we need our dictionary of words to match against.
+
 
     }
 
     public void initTiles() {
-        for (int i = 0; i < 27; i++) {
-            String buttonID = "alpha_" + alphaStrings[i];
-            int resID = getResources().getIdentifier(buttonID, "id", getPackageName());
-            buttons[i] = ((Button) findViewById(resID));
-            buttons[i].setOnClickListener((View.OnClickListener) this);
+        for (int i = 0; i < alphaStrings.length; i++) {                                                 // loop through 26 letters
+            String buttonID = "alpha_" + alphaStrings[i];                                               // buttonID example "alpha_b"
+            int resID = getResources().getIdentifier(buttonID, "id", getPackageName());                 // finds the tile that is available for interaction on the board
+            buttons[i] = ((Button) findViewById(resID));                                                // button = (Button) findViewById(R.id.alpha_b)
+            buttons[i].setOnClickListener((View.OnClickListener) this);                                 // listen for any Clicks
         }
     }
 
@@ -63,7 +72,7 @@ public class GamePlay extends AppCompatActivity {
             InputStreamReader inputs = new InputStreamReader(getAssets().open(file));                   // input the anagrams.txt file that will be in the src/main/assets directory
             BufferedReader br = new BufferedReader(inputs);                                             // Read the inputs in
             words = br.readLine().split(" ");                                                           // start counting how many words there. We can tell this by the spaces between each word
-            textViewAnagramWord.setText(words[0]);                                                      // TODO, this sets the Anagram text, what we need to do is call a function to populate the tiles
+            textViewAnagramWord.setText(words[0]);                                                      // TODO, this sets the Anagram text, what we need to do is call a function to populate the tiles when this ran (Line 84)
             words[0] = null;                                                                            // delete the existence of the word, because we don't want to be reusing it
             totalwords = words.length - 1;                                                              // get our total word count by using the length function
             textViewNumTokens.setText(wordsfound + "/" + totalwords);                                   // in the xml file textViewNumTokens will now say something like 0/152
@@ -73,29 +82,41 @@ public class GamePlay extends AppCompatActivity {
         }                                                                                               //
     }
 
-    private void resetTiles() {
+    private void getTilePlacement(String string){
+        ImageView[] tiles = {tile1, tile2, tile3, tile4, tile5, tile6, tile7};                          // put all these variables in a list. XD I can't believe this logic works TBH >_<
+        int i = 1;
+        for (char ch: string.toCharArray()){                                                            // Parse the string, get each individual letter
+            String tileSetup = "alpha_" + ch;                                                           // setup the string
+            tiles[i].setImageResource(tile[]);
+        }
+
     }
 
-    private void nextWorld() {
+    private void resetTiles() {
+                                                                                                        // TODO, make it such that tiles are clickable again
+    }
+
+    private void nextWord() {
+                                                                                                        // TODO, make it when the right arrow is clicked, we go to the next word to be solved
     }
 
     private void prevWord() {
+                                                                                                        // TODO, make it when the left arrow is clicked, we go to the previous word
     }
 
     private void submit() {
         buttonSubmit.setOnClickListener(new View.OnClickListener() {
             public void onClick(View sub) {                                                             // click to submit the typed word
-                TextView input = (TextView) findViewById(R.id.tileOutput);                              // after the click pull the string from editTextUserInput box
-                String submission = input.getText().toString();                                         // convert the pulled string
-                //
+                TextView input = (TextView) findViewById(R.id.tileOutput);                              // after the click pull the string from the tileOutput box
+                String submission = input.getText().toString();                                         // convert the string, probably redundant, but gotta make sure
                 for (int i = 0; i < words.length; i++) {                                                // going through each word that is in our list
                     if (words[i] != null) {                                                             // and as long as the list is not empty
                         if (words[i].equals(submission)) {                                              // check to see if the string is the same as the string that was typed
                             wordsfound += 1;                                                            // increment our wordsfound
-                            textViewNumTokens.setText(wordsfound + "/" + totalwords);                   // update textViewNumTokens, and let the user know that they found a correct word
+                            textViewNumTokens.setText("Words matched " + wordsfound + "/" + totalwords);// update textViewNumTokens, and let the user know that they found a correct word
                             words[i] = null;                                                            // since we found a correct word, delete it from the list
                         }
-                        if (wordsfound == totalwords) {                                                 // TODO: have the user fowarded to the Game_over layout and display score
+                        if (wordsfound == totalwords) {                                                 // TODO: have the user forwarded to the Game_over layout and display score, utilize intent here
                             //textViewUsernameTitle.setText("Great Job " + helpers.toTitleCase(username) + "!");
                             //textViewWordLabel.setText("");
                             //textViewAnagramWord.setText("You're the bestest!");
